3a46b5f49bb60e016bca394a49d4688cd8b8d23b
BertoPolo <bertopolo91@gmail.com>
2022-02-21 17:08:56 +0100

    problem with Col's

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,5 +1,56 @@
-import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 const MyFooter = function () {
-  return <Link>About</Link>;
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>LINKEDiN</Col>
+        </Row>
+        <Row className="text-muted row-cols-10">
+          <Col>
+            <Col xs={6} md={3}>
+              About
+            </Col>
+            <Col xs={6} md={3}>
+              Accessibility
+            </Col>
+            <Col xs={6} md={3}>
+              Talent Solutions
+            </Col>
+            <Col xs={6} md={3}>
+              Community Guidelines
+            </Col>
+            <Col xs={6} md={3}>
+              Careers
+            </Col>
+            <Col xs={6} md={3}>
+              Martketing Solutions
+            </Col>
+            <Col xs={6} md={3}>
+              Privacy & Terms
+            </Col>
+            <Col xs={6} md={3}>
+              Ad Choices
+            </Col>
+            <Col xs={6} md={3}>
+              Advertising
+            </Col>
+            <Col xs={6} md={3}>
+              Sales Solutions
+            </Col>
+            <Col xs={6} md={3}>
+              Mobile
+            </Col>
+            <Col xs={6} md={3}>
+              Small Business
+            </Col>
+            <Col xs={6} md={3}>
+              Safety Center
+            </Col>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 export default MyFooter;
