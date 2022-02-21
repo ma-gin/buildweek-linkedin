9858489fcc86411e3a79c442717ba8a901bfea28
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-21 18:59:29 +0100

    Interest

---
+++ src/components/InterestProfiles.jsx
@@ -0,0 +1,17 @@
+import { Button } from "react-bootstrap"
+
+export default function InterestProfiles(props) {
+  return (
+    <div className="d-flex my-2">
+      <img className="side-profile-img" src={props.img} alt="" />
+      <div className="d-flex flex-column">
+        <div className="d-flex">
+          <div className="font-weight-bold mr-2">{props.name}</div>
+          <i class="bi bi-diamond-fill"></i>
+        </div>
+        <span>{props.description}</span>
+        <span>1.697.452 follower</span>
+      </div>
+    </div>
+  )
+}
