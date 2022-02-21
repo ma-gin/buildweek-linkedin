13e2fc6da972b2dbbfbae31c78980395201f617b
BertoPolo <bertopolo91@gmail.com>
2022-02-21 14:46:18 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css"
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
-  return <div className="App">
-    
-  </div>
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
