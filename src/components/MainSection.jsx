76038e95bb21f3dcaa8cdaf12ce69064ed924cc6
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-21 15:37:40 +0100

    Infos card

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,5 +1,6 @@
 import { Row, Col } from "react-bootstrap"
 import AnalysisCard from "./AnalysisCard"
+import InfosCard from "./InfosCard"
 import ResourcesCard from "./Resourcescard"
 
 
@@ -11,6 +12,7 @@ const MainSection = () => {
 <Col md={8}>
 <AnalysisCard/>
 <ResourcesCard/>
+<InfosCard/>
 </Col>
 <Col md={4}>
 
