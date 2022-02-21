5005dd1cd49221d6bc76b819c673530fba02a4ca
Matt Ginter <mattginter@gmail.com>
2022-02-21 16:16:34 +0100

    add content

---
+++ src/components/SidebarTop.jsx
@@ -0,0 +1,14 @@
+export default function SidebarTop() {
+  return (
+    <div className="card-section">
+      <div className="d-flex justify-content-between">
+        <div>Edit public profile % URL</div>
+        <i class="bi bi-question-circle"></i>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div>Add profile in another language</div>
+        <i class="bi bi-question-circle"></i>
+      </div>
+    </div>
+  )
+}
