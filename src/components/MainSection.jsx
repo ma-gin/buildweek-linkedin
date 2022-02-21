a1cff0ea9a52cbcc15a03441cf153bce9313661d
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-21 14:13:24 +0100

    Main section

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,21 @@
+import { Row, Col } from "react-bootstrap"
+
+
+const MainSection = () => {
+
+  return(  <div class="container">
+        <Row className="m-auto" >
+
+<Col md={8}>
+
+</Col>
+<Col md={4}>
+
+</Col>
+        </Row>
+    </div>
+
+  )
+}
+
+export default MainSection
