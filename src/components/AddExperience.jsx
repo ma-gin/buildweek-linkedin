9bc5600f32b9399512f677b1b9704187edde8508
BertoPolo <bertopolo91@gmail.com>
2022-02-22 15:55:50 +0100

    super albi attack

---
+++ src/components/AddExperience.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const AddExperience = () => {
+  const [experience, setExperience] = useState({
+    role: undefined,
+    company: undefined,
+    startDate: undefined,
+    endDate: undefined,
+    description: undefined,
+    area: undefined,
+  });
+
+  const postData = async () => {
+    try {
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/62141c010448b4001511688d/experiences/",
+        {
+          method: "POST",
+          body: JSON.stringify(experience),
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjE0MWMwMTA0NDhiNDAwMTUxMTY4OGQiLCJpYXQiOjE2NDU0ODUwNTcsImV4cCI6MTY0NjY5NDY1N30.RpYP2LhIfMwWh9okgKoO9hO9xHHxMIrpOw6PlnVfviI",
+            "Content-type": "application/json",
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const grabValue = (property, value) => {
+    setExperience({ ...experience, [property]: value });
+    console.log(experience);
+  };
+
+  /* 
+  grab the info from the input,
+  assign it to the object state
+  on submit call postData
+
+  */
+
+  return (
+    <Form>
+      <Form.Group controlId="formBasicRole">
+        <Form.Label>Role</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter role"
+          onKeyUp={(e) => grabValue("role", e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicCompany">
+        <Form.Label>Company</Form.Label>
+        <Form.Control type="text" placeholder="Enter Company" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicStartDate">
+        <Form.Label>Start Date</Form.Label>
+        <Form.Control type="text" placeholder="Enter Start Date" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicEndDate">
+        <Form.Label>End Date</Form.Label>
+        <Form.Control type="text" placeholder="Enter End Date" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicDescription">
+        <Form.Label>Description</Form.Label>
+        <Form.Control type="text" placeholder="Enter Description" />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicCountry">
+        <Form.Label>Country</Form.Label>
+        <Form.Control type="text" placeholder="Enter Country" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+export default AddExperience;
--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,49 +1,65 @@
-import ExperienceCardSec from "./ExperienceCardSec"
-import { useEffect, useState } from "react"
+import ExperienceCardSec from "./ExperienceCardSec";
+import { useEffect, useState } from "react";
+import AddExperience from "./AddExperience";
 
 export default function ExperienceCard(props) {
+  const [user, setUser] = useState([]);
 
-  const [user, setUser] = useState([])
-  
+  const [selected, setSelected] = useState(false);
+
   useEffect(() => {
-    fetchData()
-  }, [])
-
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
     // const dataFetch = "https://striveschool-api.herokuapp.com/api/profile/" + props.id + "/experiences"
     try {
-      const response = await fetch( "https://striveschool-api.herokuapp.com/api/profile/62141c010448b4001511688d/experiences",
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/62141c010448b4001511688d/experiences",
         {
           headers: {
             Authorization:
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjE0MWMwMTA0NDhiNDAwMTUxMTY4OGQiLCJpYXQiOjE2NDU0ODUwNTcsImV4cCI6MTY0NjY5NDY1N30.RpYP2LhIfMwWh9okgKoO9hO9xHHxMIrpOw6PlnVfviI",
           },
         }
-      )
-      const data = await response.json()
-      setUser(data)
+      );
+      const data = await response.json();
+      setUser(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  return( 
+  return (
     <>
-  <div className="card-section p-4 mb-3">
-
-
-<div className='d-flex justify-content-between'>
-<h4 className='mb-3'> Experiences</h4>
-<div>
-<i className="bi bi-plus-lg mr-2"></i>
-<i className="bi bi-pencil"></i>
-</div>
-</div>
-{
-user.map( userex => (<ExperienceCardSec image={'http://www.50epiu.it/wp-content/uploads/2015/12/placeholder.gif'} titleText={userex.role} description={userex.description} border={{borderBottom: '1px solid gray', marginBottom:'10px'}} period={userex.startDate + ' - ' + userex.endDate} location={userex.area} key={userex._id}/>))
-}
-</div>
-</>
-  )
+      <div className="card-section p-4 mb-3">
+        <div className="d-flex justify-content-between">
+          <h4 className="mb-3"> Experiences</h4>
+          <div>
+            <i
+              className="bi bi-plus-lg mr-2"
+              onClick={() => {
+                setSelected(!selected);
+              }}
+            ></i>
+            <i className="bi bi-pencil"></i>
+          </div>
+        </div>
+        {user.map((userex) => (
+          <ExperienceCardSec
+            image={
+              "http://www.50epiu.it/wp-content/uploads/2015/12/placeholder.gif"
+            }
+            titleText={userex.role}
+            description={userex.description}
+            border={{ borderBottom: "1px solid gray", marginBottom: "10px" }}
+            period={userex.startDate + " - " + userex.endDate}
+            location={userex.area}
+            key={userex._id}
+          />
+        ))}
+        {selected === true && <AddExperience />}
+      </div>
+    </>
+  );
 }
