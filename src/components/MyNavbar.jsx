a0b251f0f47c433c9a9e9c6dfe677010b064fb7e
Matt Ginter <mattginter@gmail.com>
2022-02-22 12:01:30 +0100

    style nav

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,65 +1,84 @@
-import { Navbar, Form, Nav, FormControl, NavDropdown } from "react-bootstrap"
+import {
+  Container,
+  Navbar,
+  Form,
+  Nav,
+  FormControl,
+  NavDropdown,
+} from "react-bootstrap"
+import { Link } from "react-router-dom"
+
 const MyNavbar = function (props) {
   return (
-    <Navbar bg="light" expand="lg" className="mb-4">
-      <Navbar.Brand href="#home">
-        <i className="bi bi-linkedin"></i>
-      </Navbar.Brand>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      </Form>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto  ">
-          <Nav.Link href="#home">
-            <i className="bi bi-house-door-fill"></i>Home
-          </Nav.Link>
-          <Nav.Link href="#myNetwork">
-            <i className="bi bi-people-fill"></i>My NetWork
-          </Nav.Link>
-          <Nav.Link href="#jobs">
-            <i className="bi bi-briefcase-fill"></i>Jobs
-          </Nav.Link>
-          <Nav.Link href="#messaging">
-            <i className="bi bi-chat-dots-fill"></i>Messaging
-          </Nav.Link>
-          <Nav.Link href="#notifications">
-            <i className="bi bi-bell-fill"></i>Notifications
-          </Nav.Link>
-
-          <NavDropdown title="Me" id="profile-nav-dropdown">
-            {/* props.profileImg */}
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-
-          <NavDropdown title="Work" id="work-nav-dropdown">
-            {/* <i class="bi bi-bricks"></i> */}
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-          <a href="" className="getHired">
-            Get Hired Faster,
-            <br />
-            Try Premium Free
-          </a>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div className="navbar-full">
+      <Container>
+        <Navbar expand="lg" className="mb-4">
+          <Navbar.Brand href="#home">
+            <i className="bi bi-linkedin"></i>
+          </Navbar.Brand>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          </Form>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto text-muted">
+              <Nav.Link href="#home">
+                <i className="bi bi-house-door-fill"></i>Home
+              </Nav.Link>
+              <Nav.Link href="#myNetwork">
+                <i className="bi bi-people-fill"></i>My NetWork
+              </Nav.Link>
+              <Nav.Link href="#jobs">
+                <i className="bi bi-briefcase-fill"></i>Jobs
+              </Nav.Link>
+              <Nav.Link href="#messaging">
+                <i className="bi bi-chat-dots-fill"></i>Messaging
+              </Nav.Link>
+              <Nav.Link href="#notifications">
+                <i className="bi bi-bell-fill"></i>Notifications
+              </Nav.Link>
+              <div className="pr-2">
+                <i className="bi bi-person-circle"></i>
+                <NavDropdown title="Me" id="profile-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </div>
+              <div className="b-left pl-2">
+                <i className="bi bi-grid-3x3-gap-fill"></i>{" "}
+                <NavDropdown title="Work" id="work-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </div>
+              {/* <Link to="#" className="getHired">
+                Get Hired Faster,
+                <br />
+                Try Premium Free
+              </Link> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </div>
   )
 }
 
