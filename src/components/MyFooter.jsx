358ff10b3505f277a47228444230105a4ad1e12a
BertoPolo <bertopolo91@gmail.com>
2022-02-22 09:17:30 +0100

    almost finish the footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -9,54 +9,28 @@ const MyFooter = function () {
         <Row className="text-muted">
           <Col xs={6} md={2}>
             <p> About </p>
-            <p>Accessibility </p>
-            <p>Talent Solutions</p>
             <p>Community Guidelines</p>
-            <p>Careers</p>
-            <p>Martketing Solutions</p>
+            <p>Privacy & Terms </p>
+            <p>Sales Solutions</p>
+            <p>Safety Center</p>
           </Col>
           <Col xs={6} md={2}>
-            <p> About </p>
             <p>Accessibility </p>
-            <p>Talent Solutions</p>
-            <p>Community Guidelines</p>
+            <p> Careers </p>
+            <p>Ad Choices</p>
+            <p>Mobile</p>
             <p>Careers</p>
-            <p>Martketing Solutions</p>
           </Col>
           <Col xs={6} md={2}>
-            <p> About </p>
-            <p>Accessibility </p>
             <p>Talent Solutions</p>
+            <p> Marketing Solutons </p>
+            <p>Advertising </p>
             <p>Community Guidelines</p>
-            <p>Careers</p>
-            <p>Martketing Solutions</p>
+            <p>Advertising</p>
+            <p>Small Business</p>
           </Col>
           <Col md={2}>question</Col>
           <Col md={2}>dropdown</Col>
-          <Row>
-            <Col>other stuff</Col>
-          </Row>
-          {/* <Col xs={6} md={3}>
-            Privacy & Terms
-          </Col>
-          <Col xs={6} md={3}>
-            Ad Choices
-          </Col>
-          <Col xs={6} md={3}>
-            Advertising
-          </Col>
-          <Col xs={6} md={3}>
-            Sales Solutions
-          </Col>
-          <Col xs={6} md={3}>
-            Mobile
-          </Col>
-          <Col xs={6} md={3}>
-            Small Business
-          </Col>
-          <Col xs={6} md={3}>
-            Safety Center
-          </Col> */}
         </Row>
       </Container>
     </>
