ebc6982536309a13645a00b1a1bf6029b7d9087f
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-23 11:53:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import "./App.css";
 import MyNavbar from "./components/MyNavbar";
 import MainSection from "./components/MainSection";
 import MyFooter from "./components/MyFooter";
-import NewsMain from "./components/NewsMain";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
-      <NewsMain/>
-      {/* <MainSection />
-      <MyFooter /> */}
+      <MainSection />
+      <MyFooter />
     </div>
   );
 }
