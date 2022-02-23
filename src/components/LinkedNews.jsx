f1ec6c8df824a57ba18027818de5a66f5ae9df58
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-23 18:58:23 +0100

    Update LinkedNews.jsx

--- src/components/LinkedNews.jsx
+++ src/components/LinkedNews.jsx
@@ -8,11 +8,11 @@ const LinkedNews = (props) => {
         <h6>{props.title}</h6>
         <i className="bi bi-info-square-fill font-14"></i>
         </div>
-        <LinkedNewsSec news={'Somebody told me: Just do it'} date={'1 hour ago • 1.238 readers'}/>
-        <LinkedNewsSec news={'Somebody told me: Just do it'} date={'1 hour ago • 1.238 readers'}/>
-        <LinkedNewsSec news={'Somebody told me: Just do it'} date={'1 hour ago • 1.238 readers'}/>
-        <LinkedNewsSec news={'Somebody told me: Just do it'} date={'1 hour ago • 1.238 readers'}/>
-        <LinkedNewsSec news={'Somebody told me: Just do it'} date={'1 hour ago • 1.238 readers'}/>
+        <LinkedNewsSec news={'I want to fly high, over the rainbow, so high'} date={'1 hour ago • 1.238 readers'}/>
+        <LinkedNewsSec news={'Somebody told me: Just do it'} date={'7 hour ago • 8.572 readers'}/>
+        <LinkedNewsSec news={'Do thing you feel comfortable with or die'} date={'1 day ago • 11.238 readers'}/>
+        <LinkedNewsSec news={'This is what we call news'} date={'4 days ago • 62.491 readers'}/>
+        <LinkedNewsSec news={'Raggaeton music is the best '} date={'32 days ago • 1.001.238 readers'}/>
 
     </div>
 
