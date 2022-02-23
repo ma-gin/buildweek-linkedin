b08a650f2fc2339afef7cd6c021d2507dad2f126
Matt Ginter <mattginter@gmail.com>
2022-02-24 00:02:18 +0100

    add discover home content

--- src/components/HomeDiscover.jsx
+++ src/components/HomeDiscover.jsx
@@ -1,3 +1,28 @@
 export default function HomeDiscover() {
-  return <div className="card-section">Hello World</div>
+  return (
+    <div className="card-section font-11 mt-2">
+      <div className="collapser">
+        <h6>Recent</h6>
+        <div className="d-flex">
+          <span>People Trends in 2022</span>
+        </div>
+      </div>
+      <div className="collapser">
+        <h6>Groups</h6>
+        <div className="d-flex">
+          <span>All things</span>
+          <span>See all</span>
+        </div>
+      </div>
+      <div className="collapser">
+        <div className="d-flex">
+          <h6>Events</h6>
+          Icon
+        </div>
+        <div className="d-flex">People Trends</div>
+      </div>
+      <div className="link">Followed Hashtags</div>
+      <div className="text-center">Discover more</div>
+    </div>
+  )
 }
