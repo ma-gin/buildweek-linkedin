fab678799cc6753c72f1b9b9b080f34be94f57ab
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-23 11:33:38 +0100

    Styling

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -19,23 +19,23 @@ const MyNavbar = function (props) {
           </Form>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto text-muted">
-            <div className="mr-3">
+            <Nav className="text-muted">
+            <div className="mr-4 font-12">
                 <i className="bi bi-house-door-fill"></i>Home
             </div>
-            <div className="mr-3">
+            <div className="mr-4 font-12">
                 <i className="bi bi-people-fill"></i>NetWork
                 </div>
-           <div className="mr-3">
+           <div className="mr-4 font-12">
                 <i className="bi bi-briefcase-fill"></i>Jobs
                 </div>
-                <div className="mr-3">
+                <div className="mr-4 font-12">
                 <i className="bi bi-chat-dots-fill"></i>Message
           </div>
-          <div className="mr-3">
+          <div className="mr-4 font-12">
                 <i className="bi bi-bell-fill"></i>Notifications
                 </div>
-              <div className="mr-3">
+              <div className="mr-4 font-12">
                 <i className="bi bi-person-circle"></i>
                 <NavDropdown title="Me" id="profile-nav-dropdown" >
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -51,7 +51,7 @@ const MyNavbar = function (props) {
                   </NavDropdown.Item>
                 </NavDropdown>
               </div>
-              <div className="b-left pl-3">
+              <div className="b-left pl-4 font-12">
                 <i className="bi bi-grid-3x3-gap-fill"></i>{" "}
                 <NavDropdown title="Work" id="work-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -67,11 +67,11 @@ const MyNavbar = function (props) {
                   </NavDropdown.Item>
                 </NavDropdown>
               </div>
-              {/* <Link to="#" className="getHired">
+              <div className="text-center  ml-5 font-12 promotional">
                 Get Hired Faster,
                 <br />
                 Try Premium Free
-              </Link> */}
+                </div>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
