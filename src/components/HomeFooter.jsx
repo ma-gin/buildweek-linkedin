1141760eb3146a18f16a3151fd133f3b44057332
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-24 16:40:19 +0100

    Update HomeFooter.jsx

--- src/components/HomeFooter.jsx
+++ src/components/HomeFooter.jsx
@@ -3,46 +3,46 @@ import { Container, Row, Col, Dropdown } from "react-bootstrap";
 const HomeFooter = function () {
   return (
     <footer>
-      <Container>
+      <Container >
             <Row className="text-muted">
-            <Col md={6}>
+            <Col md={6} className="text-right">
             <p className="pointer"> About </p>
             </Col>
-            <Col md={6}>
+            <Col md={6} className="text-left">
             <p className="pointer">Accessibility </p>
             </Col>
             </Row>
             <Row className="text-muted">
-            <Col md={6}>
+            <Col md={6} className="text-right">
             <p className="pointer">Visit our Help Center</p>
             </Col>
-            <Col md={6}>
+            <Col md={6} className="text-left">
             <p className="pointer">Privacy & Terms </p>
             </Col>
             </Row>
             <Row className="text-muted">
-            <Col md={12}>
+            <Col md={12} className="text-center">
             <p className="pointer"> Marketing Solutons </p>
             </Col>
             </Row>
             <Row className="text-muted">
-            <Col md={6}>
-            <p className="pointer">Advertising</p>
+            <Col md={6} className="text-right">
+            <p className="pointer" >Advertising</p>
             </Col>
-            <Col md={6}>
+            <Col md={6} className="text-left">
             <p className="pointer">Business</p>
             </Col>
             </Row>
             <Row className="text-muted">
-            <Col md={9}>
+            <Col md={9} className="text-right">
             <p className="pointer">Download Linkedin Mobile App</p>
             </Col>
-            <Col md={3}>
+            <Col md={3} className="text-left">
             <p className="pointer">More</p>
             </Col>
             </Row>
             <Row>
-          <Col>
+          <Col className="text-center">
             <span className="linkedinIcon">Linked <i className="bi bi-linkedin"></i> </span>
             <span className="font-muted font-14">LinkedIn Corporation @2022</span>
           </Col>
